213143d361219798e2b4c12b61c90465e2f95340
Austin Luu <austinowenluu@gmail.com>
2021-01-19 22:38:44 -0500

    week8 pca & feature normalization

--- ex7/pca.m
+++ ex7/pca.m
@@ -11,6 +11,8 @@ function [U, S] = pca(X)
 U = zeros(n);
 S = zeros(n);
 
+Sigma = (1/m)*X'*X;
+[U, S, V] = svd(Sigma); 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should first compute the covariance matrix. Then, you
 %               should use the "svd" function to compute the eigenvectors
