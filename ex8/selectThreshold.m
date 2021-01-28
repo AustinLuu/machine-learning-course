414d8439100e2b6e4ecd0ed01cc917ffd3610b99
Austin Luu <austinowenluu@gmail.com>
2021-01-28 18:37:22 -0500

    Week 9 assignment update

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -12,7 +12,15 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
+    cvPredictions = (pval < epsilon);     % m x 1 
+    tp = sum((cvPredictions == 1) & (yval == 1)); % m x 1
+    fp = sum((cvPredictions == 1) & (yval == 0)); % m x 1
+    fn = sum((cvPredictions == 0) & (yval == 1)); % m x 1
     
+    prec = tp/(tp+fp); 
+    rec = tp/(tp+fn);
+
+    F1 = 2*prec*rec / (prec + rec);
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
@@ -22,19 +30,6 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
-
-
-
-
-
-
-
-
-
-
-
-
-
     % =============================================================
 
     if F1 > bestF1
