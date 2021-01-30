a5a18212ebbb3394085d822affedb7d3e4ee1dbb
Austin Luu <austinowenluu@gmail.com>
2021-01-30 00:09:20 -0500

    suggestion alg. comp

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -19,6 +19,17 @@ Theta_grad = zeros(size(Theta));
 
 Error = (X*Theta') - Y;
 J = (1/2)*sum(sum(Error.^2.*R));
+
+X_grad = (Error.*R)*Theta;
+Theta_grad = (Error.*R)'*X;
+
+Reg_term_theta = (lambda/2)*sum(sum(Theta.^2));
+Reg_term_x = (lambda/2)*sum(sum(X.^2));
+
+J = J + Reg_term_theta + Reg_term_x;
+
+X_grad = X_grad + lambda*X;             % Nm x n
+Theta_grad = Theta_grad + lambda*Theta; % Nu x n
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
